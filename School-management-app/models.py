26271346ed5752726880bb8a72da2dd4422e708c
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-12-14 16:01:39 +0300

    Defining the models

--- School-management-app/models.py
+++ School-management-app/models.py
@@ -6,3 +6,39 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///database.sqlite')
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+#Defining the Student data model
+class Student(Base):
+    tablename = 'students'
+
+student_id = Column(Integer, primary_key=True)
+name = Column(String(100), nullable=False)
+grades = Column(String(100), nullable=True)
+teacher_id = Column(Integer, ForeignKey('teachers.teacher_id'), nullable=True)
+
+teacher = relationship('Teacher', back_populates='students')
+
+#Defining the Teacher data model
+class Teacher(Base):
+    tablename = 'teachers'
+
+teacher_id = Column(Integer, primary_key=True)
+name = Column(String(100), nullable=False)
+class_name = Column(String(100), nullable=True)
+
+students = relationship('Student', back_populates='teacher')
+
+#Definin the Subject data model
+class Subject(Base):
+    tablename = 'subjects'
+
+subject_id = Column(Integer, primary_key=True)
+subject_name = Column(String(100), nullable=False)
+
+#Defining the Class data model
+class Class(Base):
+    tablename = 'classes'
+
+class_id = Column(Integer, primary_key=True)
+class_name = Column(String(100), nullable=False)
+Base.metadata.create_all(engine)
