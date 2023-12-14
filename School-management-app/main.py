92406b8074d687cf2fc36c0333b3fcb5ce817065
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-14 16:05:57 +0300

    sql engine and session

--- School-management-app/main.py
+++ School-management-app/main.py
@@ -1,3 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Student, Teacher, Subject, Class
+
+# sql engine and session
+engine = create_engine('sqlite:///database.sqlite')
+Session = sessionmaker(bind=engine)
+session = Session()
